178e26e2d866e895b3253695dce3c611e25470a2
Srinidhi-SH <nidhih8@gmail.com>
2018-12-01 11:30:52 +0530

    Graphical Models Packaged added

---
+++ graphical_models/chain_belief_propagation.py
@@ -0,0 +1,2 @@
+def calculate_prob_w_given_c():
+    print("P(W/C) = ")
---
+++ ml_playground.py
@@ -0,0 +1,4 @@
+import graphical_models.chain_belief_propagation as cbp
+
+if __name__ == '__main__':
+    cbp.calculate_prob_w_given_c()
